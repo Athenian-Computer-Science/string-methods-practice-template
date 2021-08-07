053238595cde2bdcdea7c640eb90c81c1ae2dbcd
mleich <mleich@athenian.org>
2021-08-07 15:11:04 -0700

    update exercise

---
+++ src/write_code_1.py
@@ -0,0 +1,18 @@
+#############################
+# Collaborators: (enter people or resources who/that helped you)
+# If none, write none
+#
+#
+#############################
+# Use this space to practice the methods in this lesson
+# Create a string with at least 3 words
+# Use title, upper and lower on it to compare the results
+# Create a variable that contains whitespace at the start and end
+# Then use the different stripping methods to remove it
+
+
+
+
+# Create a string and use .find() to find a character.
+# What happens if the character isn't there?
+# What if the character is there multiple times?
