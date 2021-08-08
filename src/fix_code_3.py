e34563a47e9041a6fe58c587cb491963165644e1
Megan Leich <51799170+maleich@users.noreply.github.com>
2021-08-07 21:58:39 -0700

    Create fix_code_3.py

---
+++ src/fix_code_3.py
@@ -0,0 +1,9 @@
+#############################
+# Collaborators: (enter people or resources who/that helped you)
+# If none, write none
+#
+#
+#############################
+
+location = "California"
+print(location.replace())   # Replace the first "i" with "e"
